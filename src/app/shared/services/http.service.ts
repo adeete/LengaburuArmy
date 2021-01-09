f74ad4bdd5ffa3af6b292bbb6fffd9ffffb2b508
adeete <adeete09@gmail.com>
2021-01-10 01:42:45 +0530

    minor fixes

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   HttpClient,
   HttpErrorResponse,
   HttpHeaders,
-} from '@angular/common/http';
-import { forkJoin, Observable, throwError } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
-import { Planet } from '@model/planet.model';
-import { Vehicle } from '@model/vehicle.model';
-import { AttackFalconeRequest } from '@model/attackFalconeRequest.model';
+} from "@angular/common/http";
+import { forkJoin, Observable, throwError } from "rxjs";
+import { catchError, map, mergeMap, switchMap } from "rxjs/operators";
+import { Planet } from "@model/planet.model";
+import { Vehicle } from "@model/vehicle.model";
+import { AttackFalconeRequest } from "@model/attackFalconeRequest.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HttpService {
-  private sUrl = 'https://findfalcone.herokuapp.com';
+  private sUrl = "https://findfalcone.herokuapp.com";
   constructor(private http: HttpClient) {}
 
   private handleError(error: HttpErrorResponse) {
@@ -27,12 +27,16 @@ export class HttpService {
    * @return {Observable} returns an Observable
    */
   fetchLengaburuArmy(): Observable<[Planet[], Vehicle[]]> {
-    const planets = this.http.get<Planet[]>(`${this.sUrl}/planets`).pipe(catchError(this.handleError));
-    const vehicles = this.http.get<Vehicle[]>(`${this.sUrl}/vehicles`).pipe(catchError(this.handleError));
+    const planets = this.http
+      .get<Planet[]>(`${this.sUrl}/planets`)
+      .pipe(catchError(this.handleError));
+    const vehicles = this.http
+      .get<Vehicle[]>(`${this.sUrl}/vehicles`)
+      .pipe(catchError(this.handleError));
 
     return forkJoin([planets, vehicles]);
   }
-  
+
   /**
    * This function calls the AttackFalcone API and returns the response.
    * It first fetches the token and the sends the request.
@@ -40,23 +44,20 @@ export class HttpService {
    * @return {Observable} returns an observable
    */
   findFalcone(oAttackRequest: AttackFalconeRequest): Observable<any> {
-    const headers = new HttpHeaders({ Accept: 'application/json' });
+    const headers = new HttpHeaders({ Accept: "application/json" });
     return this.http
       .post(`${this.sUrl}/token`, null, {
         headers: headers,
       })
       .pipe(
         catchError(this.handleError),
-        map((token: any) => {
+        switchMap((token: any) => {
           oAttackRequest.token = token.token;
-          headers.append('Content-Type', 'application/json');
-          return oAttackRequest;
-        }),
-        mergeMap((data: AttackFalconeRequest) =>
-          this.http.post(`${this.sUrl}/find`, data, { headers: headers }).pipe(
-            catchError(this.handleError)
-          )
-        )
+          headers.append("Content-Type", "application/json");
+          return this.http
+            .post(`${this.sUrl}/find`, oAttackRequest, { headers: headers })
+            .pipe(catchError(this.handleError));
+        })
       );
   }
 }
